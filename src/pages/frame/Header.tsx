c5c557d4fcd5661bcaa3f520a8012abed181dc2f
Weronika Klich <klichweronika9@gmail.com>
2021-09-08 20:37:21 +0200

    Add logo icon, fix styles

--- src/pages/frame/Header.tsx
+++ src/pages/frame/Header.tsx
@@ -1,9 +1,11 @@
 import { ReactElement } from "react";
 import styled from "styled-components";
+import LogoImage from "../../assets/images/logo.png";
 
 function Header(): ReactElement {
   return (
     <HeaderContainer>
+      <Logo src={LogoImage} alt="logo" />
       <ContactUs>
         <div>call us</div>
         <div>phone number</div>
@@ -14,20 +16,29 @@ function Header(): ReactElement {
 
 const HeaderContainer = styled.div`
   display: flex;
-  flex: 1;
-  background-color: silver
+  flex: 0.5;
+  justify-content: space-between;
+  background-color: #eeeeee;
   box-sizing: border-box;
-  padding: 0 5rem;
-
+  align-items: center;
+  padding: 0rem 4rem;
 `;
 
 const ContactUs = styled.a`
-  margin-right: 5px;
+  cursor: pointer;
+  margin-right: 4px;
   display: flex;
-  padding: 5px;
+  color: #2f79ce;
+  text-transform: uppercase;
+  padding: 10px;
   div {
-    margin-right: 5px;
+    margin-right: 10px;
   }
+`;
+
+const Logo = styled.img`
+  max-width: 7rem;
+  padding: 1%;
 `;
 
 export default Header;
