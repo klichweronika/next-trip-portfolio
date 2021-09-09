8acd366588647290c1228bc36d412dfe0bc94837
Weronika Klich <klichweronika9@gmail.com>
2021-09-10 00:54:34 +0200

    Add locale file and fix index.html

---
+++ src/common/Locale.tsx
@@ -0,0 +1,35 @@
+type Texts = {
+  subject: string;
+  comment: string;
+  sendButton: string;
+  emailUs: string;
+  requiredInfo: string;
+  contactUs: string;
+  privacyPolicy: string;
+  allRightsReserved: string;
+  callUs: string;
+  placeholderEmail: string;
+  placeholderSubject: string;
+  placeholderComment: string;
+  invalidFormat: string;
+  tooManyCharacters: string;
+  unknownDataError: string;
+};
+
+export const Locale: Texts = {
+  subject: "subject",
+  comment: "comment",
+  sendButton: "send",
+  emailUs: "Email Us",
+  callUs: "Call Us",
+  requiredInfo: "This value is required.",
+  contactUs: "Contact Us",
+  privacyPolicy: "Privacy policy",
+  allRightsReserved: "@ NextTrip. All rights reserved.",
+  placeholderEmail: "Enter Your Email",
+  placeholderSubject: "Enter Subject",
+  placeholderComment: "Write your comment...",
+  invalidFormat: "Format of provided data is invalid.",
+  tooManyCharacters: "There are too many characters.",
+  unknownDataError: "Unknown input data error occured.",
+};
